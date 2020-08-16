18dc6cba2296577adfe2da41ff9b6c6305bbef2e
Pangjing-Wu <pangjing.wu@outlook.com>
2020-08-16 22:25:17 +0800

    improve main script

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 @Author: Pangjing Wu
-Last Update: 2019-09-29
+Last Update: 2020-08-16
 """
+import argparse
 import os
 
 import numpy as np
@@ -13,81 +14,72 @@ from sklearn.metrics import classification_report, silhouette_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
 
-from core.dataloader import TimeSeries
-from core.model import Hierarchy, period2daily, rolling_fit_pred
-from transformer import senticnet
-
-np.random.seed(1)
+from core.generator import weekly
+from core.model import Hierarchy, grid_search_hierarchy, rolling_fit_pred
+from transformer import daily2period, period2daily
 
 
-def grid_search_hierarchy(data, n_class, taus):
-    scores = []
-    for tau in taus:
-        score = []
-        for n in n_class:
-            X = data.weekly(senticnet,tau)[0]
-            X = StandardScaler().fit_transform(X)
-            clster = Hierarchy(n, method='ward', criterion='maxclust').fit(X)
-            score.append(clster.silhouette_score_)
-        scores.append(score)
-    scores   = np.array(scores).T
-    best_tau = taus[np.argmax(score) % len(taus)]
-    best_n   = n_class[np.argmax(score) // len(taus)]
-    print('get best cluster parameters: tau = %d, n = %d' % (best_tau, best_n))
-    return best_tau, best_n
+def parse_args():
+    parser = argparse.ArgumentParser(description= 'market style rotation analysis')
+    parser.add_argument('-s', '--stock', required=True, type=str, help='stock id')
+    parser.add_argument('-i', '--data_dir', required=True, type=str, help='direction of data file')
+    parser.add_argument('-o', '--save_dir', required=True, type=str, help='direction of output file')
+    parser.add_argument('--pre_n', default=250, type=int, help='use previous n days data as support set')
+    parser.add_argument('--split', default=0.8, type=float, help='train test split')
+    return parser.parse_args()
 
-def main(stock):
-    n = 3
-    tau = 4
-    split = 0.8
-    pre_n = 250
-    cols = ['Date', 'pos', 'neu', 'neg', 'ple', 'att', 'sen',
-            'apt', 'Volume', 'ma10', 'ptc_chg', 'rsi12', 'macd', 'mfi']
+
+if __name__ == '__main__':
+    params = parse_args()
+    np.random.seed(1)
+    
+    XCOLS  = ['Date', 'pos', 'neu', 'neg', 'ple', 'att', 'sen',
+              'apt', 'Volume', 'ma10', 'ptc_chg', 'rsi12', 'macd', 'mfi']
+    YCOL   = []
+
+    # load data and periodization.
+    data    = pd.read_csv()
+    data_p  = [p for p in weekly(data, tau)]
+    date_p  = [dat.index.values.tolist() for dat in data_p]
+    data_p  = [daily2period(p) for p in data_p]
+    i_split = int(len(data_p) * params.split)
+
+    # spread features and label.
+    y_p = np.array([dat[YCOL] for dat in data_p])
+    X_p = np.array([dat[XCOLS] for dat in data_p])
+    X_p = StandardScaler().fit(X_p[:i_split]).transform(X_p)
+    
+    # cluster.
+    taus    = np.arange(1,8)
+    n_class = np.arange(2,10)
+    n, tau  = grid_search_hierarchy(X_p, taus=taus, n_class=n_class)
     cluster = Hierarchy(n_clusters=n, method='ward', criterion='maxclust')
+    style_p = cluster.fit(X_p[:i_split]).predict(X_p)
+    style   = period2daily(style_p, date_p)
+    
+    # classification.
+    X = data[XCOLS].values
+    y = np.where(data[YCOL] >= 0, 1, -1)
+
     clf_parameters = {'C': np.logspace(0,3,10), 'gamma': np.logspace(-2,2,10)}
     clf = GridSearchCV(svm.SVC(kernel='rbf', class_weight='balanced'),
                     clf_parameters, iid=True, cv=5, n_jobs=-1,
                     scoring='f1_weighted', return_train_score=True)
 
-    # read and preprocess data
-    data = TimeSeries('data/%s.csv' % stock, usecols=cols)
-    savepath = 'results/' 
-    os.makedirs(savepath, exist_ok=True)
+    # train baseline.
+    y_pred = rolling_fit_pred(clf, X[:-1], y[1:], style[:-1],
+                             split=params.split, pre_n=params.pre_n, z_enable=False)
+    results = pd.DataFrame(index=X.index[-len(y_pred):])
+    results['baseline'] = y_pred
+    print('--- baseline results ---')
+    print(classification_report(y[-len(y_pred):], y_pred), end='\n\n')
 
-    # cluster
-    X_p, date = data.weekly(senticnet ,tau)
-    i_split = int(X_p.shape[0] * split)
-    # make normalization based on train set
-    X_p = StandardScaler().fit(X_p[:i_split]).transform(X_p)
-    # make cluster based on train set
-    style_p = cluster.fit(X_p[:i_split]).predict(X_p)
-    style = period2daily(style_p, date)
+    # train our method.
+    y_pred = rolling_fit_pred(clf, X[:-1], y[1:], style[:-1],
+                             split=params.split, pre_n=params.pre_n, z_enable=True)
+    result['ours'] = y_pred
+    print('--- our method results ---')
+    print(classification_report(y[-len(y_pred):], y_pred), end='\n\n')
 
-    # classification
-    X = data.X
-    y = np.where(X['ptc_chg'] >= 0, 1, -1)
-    
-    
-    y_pred, cv_results = rolling_fit_pred(clf, X[:-1], y[1:], style[:-1],
-                                split=split, pre_n=pre_n, z_enable=True)
-    #print(classification_report(y[-len(y_pred):], y_pred))
-    df = pd.DataFrame({'date':X.index[-len(y_pred):].values, 'y_pred':y_pred})
-    df.to_csv(savepath + stock + '_y.csv',index=False)
-    cv_results.to_csv(savepath + stock + "_CVresult.csv", index=False)
-    with open(savepath+stock+'.txt','w') as file:
-        file.write(classification_report(y[-len(y_pred):], y_pred))
-        file.write('\nbest param:\n%s\nbest score: %.2f\n' % (clf.best_params_, clf.best_score_))
-
-    y_pred, cv_results = rolling_fit_pred(clf, X[:-1], y[1:], style[:-1],
-                                split=split, pre_n=pre_n, z_enable=False)
-    #print(classification_report(y[-len(y_pred):], y_pred))
-    df = pd.concat([df, pd.DataFrame({'baseline':y_pred})])
-    df.to_csv(savepath+stock+'_y.csv',index=False)
-    cv_results.to_csv(savepath + stock + "_CVresult.baseline.csv", index=False)
-    with open(savepath+stock+'.txt','a') as file:
-        file.write(classification_report(y[-len(y_pred):], y_pred))
-        file.write('\nbest param:\n%s\nbest score: %.2f\n' % (clf.best_params_, clf.best_score_))
-if __name__ == '__main__':
-    import sys
-    argv = sys.argv[1:]
-    main(*argv)
+    # save all prediction labels.
+    result.to_csv(os.path.join(params.o, params.s+'.csv'))
